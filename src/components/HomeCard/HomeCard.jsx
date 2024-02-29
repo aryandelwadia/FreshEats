01cddb5dc56d8369e75db582d7e48d14b668b273
Aryan Delawadia <aryandelwadia@gmail.com>
2024-03-01 01:39:00 +0530

    animation

--- src/components/HomeCard/HomeCard.jsx
+++ src/components/HomeCard/HomeCard.jsx
@@ -1,8 +1,8 @@
 import './homecard.css'
 
-export default function HomeCard({ img, heading, content}){
+export default function HomeCard({ img, heading, content }){
     return <>
-    <div className="bghc text-center w-1/3 rounded-2xl mb-20">
+    <div className="bghc text-center w-1/3 rounded-2xl mb-20"  >
         <img src={img} alt="" className=""/>
         <div className='cantora-one-regular csgreen text-4xl mt-5'>{heading}</div>
         <div className='px-5 pb-5 text-xl'>{content}</div>
--- src/components/InfoSection/Info.jsx
+++ src/components/InfoSection/Info.jsx
@@ -1,38 +1,63 @@
 import HomeCard from '../HomeCard/HomeCard';
 import './info.css';
+import { useEffect } from 'react';
 
 export default function Info(){
+    
+    useEffect(()=>{
+
+        const observer = new IntersectionObserver((e)=>{
+            e.forEach((entry)=>{
+                if(entry.isIntersecting){
+                    entry.target.classList.add('shw');
+                }
+                else{
+                    entry.target.classList.remove('shw');
+                }
+            });
+        });
+    
+        const hidelement = document.querySelectorAll('.hid');
+        hidelement.forEach((el)=>{
+            observer.observe(el);
+        });
+    },[]);
+    
     return <>
-        <div className="flex justify-between">
+        <div className="flex justify-between" >
             <div className="ml-40 my-40 cantora-one-regular">
-                <div className=' font-bold text-5xl w-1/2'>
+                <div className=' font-bold text-5xl w-1/2 hid' >
                     We Believe In Purity,<br></br><br></br>
                     We Believe in Nature
                 </div>
                 <br />
-                <div className=' w-1/2 text-xl average-sans-regular '>
+                <div className=' w-1/2 text-xl average-sans-regular hid ' >
                 Apples. Oranges. Limes. Lemons. Guavas. Carrots. Cucumbers. Jicamas. Cauliflowers. Brussels sprouts. Shallots. Japanese eggplants. Asparagus. Artichokes—Jerusalem artichokes, too. Radishes. Broccoli. Baby broccoli. Broccolini. Bok choy. Scallions. Ginger. Cherries. Raspberries. Cilantro. Parsley. Dill. 
                 </div>
             </div>
-            <img src="src/assets/Union.png" alt="" className='w-1/3 h-1/3 '/>
+            <img src="src/assets/Union.png" alt="" className='w-1/3 h-3/5 hid'/>
         </div>
-
-        <div className=" flex justify-between">
-            <img src="src/assets/Strawberry.png" alt="" className='w-1/3 h-1/3 '/>
-            <div className="my-40 cantora-one-regular text-right">
-                <div className=' font-bold text-5xl mr-40 '>
+        <br></br>
+        <br></br>
+        <div className=" flex justify-between" >
+            <img src="src/assets/Strawberry.png" alt="" className='w-1/3 h-1/3 hid' />
+            <div className="my-40 cantora-one-regular text-right" >
+                <div className=' font-bold text-5xl mr-40 hid'>
                     An Apple A Day,<br></br><br></br>
                     Keeps Doctor Away
                 </div>
                 <br />
-                <div className='  text-xl average-sans-regular text-right mr-40 ml-96'>
+                <div className='  text-xl average-sans-regular text-right mr-40 ml-96 hid' >
                 Discover the vibrant world of fresh fruits and vegetables, rich in essential vitamins, minerals, and antioxidants. Embrace their disease-fighting properties, fiber-rich goodness, and hydrating benefits for optimal health and vitality. With endless flavors and environmental benefits, make fresh produce a delicious cornerstone of your daily diet.
                 </div>
             </div>
         </div>
         <br />
+        <br />
+        <br />
+        <br />
 
-        <div className='flex justify-evenly'>
+        <div className='flex justify-evenly hid homecards'>
             <HomeCard img="src/assets/Women.png" heading="Trusted By 4Lac People" content="Welcome to our trusted marketplace for fresh, quality produce! Explore our selection of vibrant fruits and vegetables, handpicked for families seeking wholesome goodness." />
             <HomeCard img="src/assets/bulk.png" heading="Bulk Orders Accepted" content="Bulk orders welcomed! Stock up on fresh produce with ease. Contact us today to inquire about special pricing and arrangements." />
         </div>
