c841451e18bb49a928342458833b1639491a85bf
Aryan Delawadia <aryandelwadia@gmail.com>
2024-05-01 00:04:38 +0530

    partial socketio

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,14 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
-const multer = require('multer');
 
 const userRouter = require('./router/userRouter');
 const sellerRouter = require('./router/sellerRouter');
 const itemRouter = require('./router/itemRouter')
 
 const app = express();
-
-app.use(express.json());
-app.listen(3000, ()=>{
-    console.log('listening on 3000');
-})
-
-app.use(cookieParser());
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -23,27 +16,24 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
+app.use(express.json());
 
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }
+});
+
+io.on("connection", (socket)=>{});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+app.use(cookieParser());
 app.use('/user', userRouter);
 app.use('/seller', sellerRouter);
 app.use('/item', itemRouter);
-
-// const storage = multer.diskStorage({
-//     destination: function(req, file, cd){
-//         return cd(null, `./upload`);
-//     },
-//     filename: function(req, file, cd){
-//         return cd(null, `hi`);
-//     },
-// })
-
-// const upload = multer({ storage });
-
-
-// app.post('/user/uploadimage', upload.single('profileimage'), function(req, res, next){
-    
-//     console.log(req.file);
-//     res.json({
-//         message: 'image uploaded'
-//     })
-// });
